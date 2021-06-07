46c21217b4322c6e4a88b0a10373e09d598479e6
Igor-Leontyev <79319381+Igor-Leontyev@users.noreply.github.com>
2021-06-07 17:59:37 +0300

    task four

---
+++ app/src/main/java/com/crybz/secondtask/choice_activity.java
@@ -0,0 +1,13 @@
+package com.crybz.secondtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class choice_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_activity);
+    }
+}
